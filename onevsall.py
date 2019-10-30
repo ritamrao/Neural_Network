a277ea408b9fa963ba3d2f49b0f10870b0267df7
ritamrao <54271550+ritamrao@users.noreply.github.com>
2019-10-30 19:38:28 +0530

    Add files via upload

---
+++ onevsall.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+
+mat=loadmat("ex3data1.mat")
+X=mat["X"]
+y=mat["y"]
+
+def sigmoid(z):
+    return 1/ (1 + np.exp(-z))
+
+def lrCostFunction(theta, X, y, Lambda):
+    """
+    Takes in numpy array of theta, X, y, and float lambda to compute the regularized logistic cost function 
+    """
+    
+    m=len(y)
+    predictions = sigmoid(X @ theta)
+    error = (-y * np.log(predictions)) - ((1-y)*np.log(1-predictions))
+    cost = 1/m * sum(error)
+    regCost= cost + Lambda/(2*m) * sum(theta[1:]**2)
+    
+    # compute gradient
+    j_0= 1/m * (X.transpose() @ (predictions - y))[0]
+    j_1 = 1/m * (X.transpose() @ (predictions - y))[1:] + (Lambda/m)* theta[1:]
+    grad= np.vstack((j_0[:,np.newaxis],j_1))
+    return regCost[0], grad
+
+theta_t = np.array([-2,-1,1,2]).reshape(4,1)
+X_t =np.array([np.linspace(0.1,1.5,15)]).reshape(3,5).T
+X_t = np.hstack((np.ones((5,1)), X_t))
+y_t = np.array([1,0,1,0,1]).reshape(5,1)
+J, grad = lrCostFunction(theta_t, X_t, y_t, 3)
+print("Cost:",J,"Expected cost: 2.534819")
+print("Gradients:\n",grad,"\nExpected gradients:\n 0.146561\n -0.548558\n 0.724722\n 1.398003")
+
+def gradientDescent(X,y,theta,alpha,num_iters,Lambda):
+    """
+    Take in numpy array X, y and theta and update theta by taking num_iters gradient steps
+    with learning rate of alpha
+    
+    return theta and the list of the cost of theta during each iteration
+    """
+    
+    m=len(y)
+    J_history =[]
+    
+    for i in range(num_iters):
+        cost, grad = lrCostFunction(theta,X,y,Lambda)
+        theta = theta - (alpha * grad)
+        J_history.append(cost)
+    
+    return theta , J_history
+def oneVsAll(X, y, num_labels, Lambda):
+    """
+    Takes in numpy array of X,y, int num_labels and float lambda to train multiple logistic regression classifiers
+    depending on the number of num_labels using gradient descent. 
+    
+    Returns a matrix of theta, where the i-th row corresponds to the classifier for label i
+    """
+    m, n = X.shape[0], X.shape[1]
+    initial_theta = np.zeros((n+1,1))
+    all_theta = []
+    all_J=[]
+    # add intercept terms
+    
+    X = np.hstack((np.ones((m,1)),X))
+    
+    for i in range(1,num_labels+1):
+        theta , J_history = gradientDescent(X,np.where(y==i,1,0),initial_theta,1,300,Lambda)
+        all_theta.extend(theta)
+        all_J.extend(J_history)
+    return np.array(all_theta).reshape(num_labels,n+1), all_J
+
+def predictOneVsAll(all_theta, X):
+    """
+    Using all_theta, compute the probability of X(i) for each class and predict the label
+    
+    return a vector of prediction
+    """
+    m= X.shape[0]
+    X = np.hstack((np.ones((m,1)),X))
+    all_theta = np.asarray(all_theta)  
+    
+    predictions = X @ all_theta.T
+    return np.argmax(predictions,axis=1)+1
+
+#all_theta= oneVsAll(X, y, num_labels, Lambda)
+#all_theta = oneVsAll(X, y, 10, 1)
+all_theta, all_J = oneVsAll(X, y, 10, 0.1)
+pred = predictOneVsAll(all_theta, X)
+print("Training Set Accuracy:",sum(pred[:,np.newaxis]==y)[0]/5000*100,"%")
